216e0b5e338226d01a908ad0b4d430b4ac3fbeca
Matthew Knight <mattknight.sa@gmail.com>
2019-10-31 22:02:53 -0500

    Thurs update

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ restartButton.style.display = "none";
 var answersCorrect = 0;
 var answersIncorrect = 0;
 var usersArray = [];
+var storedUsers;
 var questions = [
     
     {
@@ -65,124 +66,129 @@ var questions = [
    
   ];
 
+  startButton.addEventListener("click", function() {
+    renderPage()
+    clearAnswer()
+    timer()
+});
 
-    startButton.addEventListener("click", function() {
-        renderPage()
-        clearAnswer()
-        timer()
-    });
+submit.addEventListener("click", function(event) {
+event.preventDefault();
+var selectedOption = document.querySelector('input[type=radio]:checked');
+var answerQ = selectedOption.value;
 
-    submit.addEventListener("click", function(event) {
-    event.preventDefault();
-    var selectedOption = document.querySelector('input[type=radio]:checked');
-    var answerQ = selectedOption.value;
+      if(questions[x].answer == answerQ ) {   
+          answersCorrect++  
+          console.log(answersCorrect, answersIncorrect)
+      } else {
+        time = time - 10;
+        answersIncorrect++
+        console.log(answersCorrect, answersIncorrect)
+      }
+  renderPage()
+  clearAnswer()
+  x++
+  });
+
+function renderPage() {
+    i ++
+    if(i == 5) {
+        done();
+    }else {
+    question.textContent = questions[i].title;
+    paragraph.textContent = "";
+    startButton.style.display = "none";
+    answer1.style.display = "inline";
+    answer2.style.display = "inline";
+    answer3.style.display = "inline";
+    answer4.style.display = "inline";
+    answer5.style.display = "inline";
+    submit.style.display = "block";
+    answer1.nextSibling.textContent = " " + questions[i].choices[0];
+    answer2.nextSibling.textContent = " " + questions[i].choices[1];
+    answer3.nextSibling.textContent = " " + questions[i].choices[2];
+    answer4.nextSibling.textContent = " " + questions[i].choices[3];
+    answer5.nextSibling.textContent = " " + questions[i].choices[4];
+    }}
+
+
+
+function timer() {
+      interval = setInterval(function(){
+      time--;
+      timeDisplay.textContent = time;
+      if(time === 0) {
+        pause();
+        done();
+      }
+      if(time < 10) {
+      timeDisplay.style.color = "red";
+      timeDisplay.style.fontSize = "25px";
+      timeDisplay.style.padding = "5px";
+      }
+    },1000);
+  }
+
+function pause(){
+    clearInterval(interval);
+    // time = time - 10;
+    timeDisplay.textContent = time;
     
-          if(questions[x].answer == answerQ ) {   
-              answersCorrect++  
-              console.log(answersCorrect, answersIncorrect)
-          } else {
-            time = time - 10;
-            answersIncorrect++
-            console.log(answersCorrect, answersIncorrect)
-          }
-      renderPage()
-      clearAnswer()
-      x++
-      });
+  }
 
-    function renderPage() {
-        i ++
-        if(i == 5) {
-            done();
-        }else {
-        question.textContent = questions[i].title;
-        paragraph.textContent = "";
-        startButton.style.display = "none";
-        answer1.style.display = "inline";
-        answer2.style.display = "inline";
-        answer3.style.display = "inline";
-        answer4.style.display = "inline";
-        answer5.style.display = "inline";
-        submit.style.display = "block";
-        answer1.nextSibling.textContent = " " + questions[i].choices[0];
-        answer2.nextSibling.textContent = " " + questions[i].choices[1];
-        answer3.nextSibling.textContent = " " + questions[i].choices[2];
-        answer4.nextSibling.textContent = " " + questions[i].choices[3];
-        answer5.nextSibling.textContent = " " + questions[i].choices[4];
-        }}
+function done() {
+      question.textContent = "Finished! Final score: " + answersCorrect;
+      paragraph.textContent = " ";
+      answers.textContent = "";
+      initialsForm.style.display = "inline";
+      initialsButton.style.display = "inline";
+      // initials = document.querySelector("#initials-input").value;
+      // localStorage.setItem("initials", initials);
+      clearAnswer()
+      pause()
+  }
+
+  initialsButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    initials = document.querySelector("#initials-input").value;
+    user = {
+      initials: initials,
+      score: answersCorrect
+    };
+      usersArray.push(user);
+      window.localStorage.setItem('users', JSON.stringify(usersArray));
+      var test = JSON.parse(localStorage.getItem("users"))
+      console.log(test)
+      
+      highScore()
+
+  })
+  
+function clearAnswer() {
+    answer1.checked = false;
+    answer2.checked = false;
+    answer3.checked = false;
+    answer4.checked = false;
+  } 
 
  
+
+  function highScore() {
+    question.textContent = "High Score";
+    initialsForm.value = user.score;
+    initialsButton.style.display = "none";
+    restartButton.style.display = "block";
+    }
+
+    $(restartButton).click(function(){
+      location.reload(true);
+  });
+
     
-    function timer() {
-          interval = setInterval(function(){
-          time--;
-          timeDisplay.textContent = time;
-          if(time === 0) {
-            pause();
-            done();
-          }
-          if(time < 10) {
-          timeDisplay.style.color = "red";
-          timeDisplay.style.fontSize = "25px";
-          timeDisplay.style.padding = "5px";
-          }
-        },1000);
-      }
 
-    function pause(){
-        clearInterval(interval);
-        // time = time - 10;
-        timeDisplay.textContent = time;
-        
-      }
-
-    function done() {
-          question.textContent = "Finished! Final score: " + answersCorrect;
-          paragraph.textContent = " ";
-          answers.textContent = "";
-          initialsForm.style.display = "inline";
-          initialsButton.style.display = "inline";
-          // initials = document.querySelector("#initials-input").value;
-          // localStorage.setItem("initials", initials);
-          clearAnswer()
-          pause()
-      }
-
-      initialsButton.addEventListener("click", function(event) {
-        event.preventDefault();
-        initials = document.querySelector("#initials-input").value;
-        user = {
-          initials: initials,
-          score: answersCorrect
-        };
-        
-          localStorage.setItem("user",JSON.stringify(user));
-          usersArray.push(user);
-          localStorage.setItem("users",JSON.stringify(usersArray));
-          
-          highScore()
-
-      })
-      
-    function clearAnswer() {
-        answer1.checked = false;
-        answer2.checked = false;
-        answer3.checked = false;
-        answer4.checked = false;
-      } 
-
-      
-
-      function highScore() {
-        question.textContent = "High Score";
-        initialsForm.value = user.score;
-        initialsButton.style.display = "none";
-        restartButton.style.display = "block";
-        }
-
-        $(restartButton).click(function(){
-          location.reload(true);
-      });
+      //   $(restartButton).click(function(){
+      //     location.reload(true);
+      // });
 
     //  restartButton.addEventListener("click", function() {
 
